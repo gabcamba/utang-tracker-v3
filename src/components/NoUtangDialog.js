90158807629ec6fe76b5576beacd50e2a589dc81
meinardxd <johnmeinardmorelos@gmail.com>
2023-12-13 19:52:41 +0800

    added group jump gif and removed previous one

--- src/components/NoUtangDialog.js
+++ src/components/NoUtangDialog.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { NO_UTANG_FOUND } from "../constants";
-import mimi from "../gifs/Mimi-GIFV2-lg.gif";
-import peach from "../gifs/Peach-GIFV2-lg.gif";
-import yuki from "../gifs/Yuki-GIFV2-lg.gif";
-import zoe from "../gifs/Zoe-GIF-lg.gif";
+import pixelpets from "../gifs/Group-Jump-Final.gif";
 
 const NoUtang = () => {
   return (
     <>
       <div>
-        <img alt="peach" style={{ height: 40, width: 40 }} src={peach} />
-        <img alt="zoe" style={{ height: 40, width: 40, marginBottom: 2 }} src={zoe} />
-        <img alt="mimi" style={{ height: 40, width: 40 }} src={mimi} />
-        <img alt="yuki" style={{ height: 40, width: 40, marginBottom: 2 }} src={yuki} />
+        <img alt="pixelpets" style={{ height: 40, width: 125 }} src={pixelpets} />
       </div>
 
       <span>{NO_UTANG_FOUND}</span>
